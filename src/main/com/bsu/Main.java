8662ac7eafcb157841d93ebc99f6f1f253d54688
Alina Mlynorik <fpm.mlynorik@bsu.by>
2020-11-01 21:29:19 +0300

    columns names and commands as constants

--- src/main/com/bsu/Main.java
+++ src/main/com/bsu/Main.java
@@ -19,15 +19,13 @@ import java.util.regex.Pattern;
 public class Main {
     public static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     private static final String LOG_FILE_NAME = "logs/logfile.txt";
-    public static String request_output_file = "request.txt";
 
     public static void main(String[] args) {
-        if (args.length == 3) {
+        if (args.length >= 3) {
             try (Scanner scanner = new Scanner(System.in);
                  Scanner inputFile = new Scanner(new FileReader(args[0]));
                  Scanner inputRequestFile = new Scanner(new FileReader(args[2]));
-                 FileWriter outputFile = new FileWriter(args[1]);
-                 FileWriter outputRequestFile = new FileWriter(request_output_file)) {
+                 FileWriter outputFile = new FileWriter(args[1])) {
                 {
                     LOGGER.setLevel(Level.FINE);
                     FileHandler fh = new FileHandler(LOG_FILE_NAME, true);
@@ -51,8 +49,6 @@ public class Main {
                     processing(request, companyList);
                 }
 
-                //getInfoByRequest(scanner, companyList, outputFile);
-                //String sql = "select * from company_table where short_name=\"short name\" employee_num between 90 and 181";
 
 
             } catch (IOException ex) {
@@ -66,56 +62,57 @@ public class Main {
     }
 
     static void processing(String str, List<Company> companyList) throws CustomException, ParseException {
-        String[] data = str.toLowerCase().split("(=\".*\" )|(=\".*\"$)|[ ,]+");
+        String[] data = str.toLowerCase().split("(=\".*\" )|(=\".*\"$)|[ ]+");
         Pattern r = Pattern.compile("\".*?\"");
         Matcher m = r.matcher(str);
-        if (data[0].equals("select") && data[1].equals("*") && data[2].equals("from")) {
+        if (data.length>=4&&data[0].equals(SQLRequests.COMMAND_SELECT) && data[1].equals("*") && data[2].equals(SQLRequests.COMMAND_FROM)) {
             List<Company> result = companyList;
-            if (data[3].equals("company_table")) {
+            if (data[3].equals(SQLRequests.TABLE_NAME)) {
                 int i = 4;
-                if (data[i].equals("where")) {
-                    for (i = 5; i < data.length && !data[i].equals("into"); ++i)
+                if (data[i].equals(SQLRequests.COMMAND_WHERE)) {
+                    for (i = 5; i < data.length && !data[i].equals(SQLRequests.COMMAND_INTO); ++i)
                         switch (data[i]) {
-                            case "short_name":
+                            case SQLRequests.COLUMN_SHORT_NAME:
                                 if(!m.find())throw new CustomException("Invalid command format");
                                 result = Query.findByShortName(result, str.substring(m.start() + 1, m.end() - 1));
                                 break;
-                            case "industry":
+                            case SQLRequests.COLUMN_INDUSTRY:
                                 if(!m.find())throw new CustomException("Invalid command format");
                                 result = Query.findByIndustry(result, str.substring(m.start() + 1, m.end() - 1));
                                 break;
-                            case "activity":
+                            case SQLRequests.COLUMN_ACTIVITY:
                                 if(!m.find())throw new CustomException("Invalid command format");
                                 result = Query.findByActivity(result, str.substring(m.start() + 1, m.end() - 1));
                                 break;
-                            case "foundation_date":
-                                if (i + 4 < data.length && data[++i].equals("between") && data[i + 2].equals("and")) {
+                            case SQLRequests.COLUMN_FOUNDATION_DATE:
+                                if (i + 4 < data.length && data[++i].equals(SQLRequests.COMMAND_BETWEEN) && data[i + 2].equals(SQLRequests.COMMAND_AND)) {
                                     Date date1 = Company.format.parse(data[++i]);
                                     Date date2 = Company.format.parse(data[i + 2]);
                                     i = i + 2;
                                     result = Query.findByFoundationDate(result, date1, date2);
                                 } else throw new CustomException("Invalid command format");
                                 break;
-                            case "employee_num":
-                                if (i + 4 < data.length && data[++i].equals("between") && data[i + 2].equals("and")) {
+                            case SQLRequests.COLUMN_EMPLOYEE_NUM:
+                                if (i + 4 < data.length && data[++i].equals(SQLRequests.COMMAND_BETWEEN) && data[i + 2].equals(SQLRequests.COMMAND_AND)) {
                                     int num1 = Integer.parseInt(data[++i]);
                                     int num2 = Integer.parseInt(data[i + 2]);
                                     i = i + 2;
                                     result = Query.findByEmployeeNumber(result, num1, num2);
                                 } else throw new CustomException("Invalid command format");
                                 break;
-                            case "and":
+                            case SQLRequests.COMMAND_AND:
                                 break;
                             default:
                                 throw new CustomException("Invalid command format");
                         }
 
                 }
-                if (i < data.length && data[i].equals("into") && data[++i].equals("outfile")) {
+                String outputFileName = "request.txt";
+                if (i < data.length && data[i].equals(SQLRequests.COMMAND_INTO) && data[++i].equals(SQLRequests.COMMAND_OUTFILE)) {
                     if (i + 1 >= data.length) throw new CustomException("Invalid command format");
-                    Main.request_output_file = data[++i];
+                    outputFileName = data[++i];
                 }
-                try (FileWriter of = new FileWriter(Main.request_output_file,true)) {
+                try (FileWriter of = new FileWriter(outputFileName,true)) {
                     if (result.isEmpty()) {
                         of.write(("None\n"));
                     } else {
@@ -135,91 +132,4 @@ public class Main {
         }
     }
 
-    static void getInfoByRequest(Scanner scanner, List<Company> companyList, FileWriter of) throws CustomException {
-        int key;
-        List<Company> result;
-        while (true) {
-            printMenu();
-            System.out.println("Enter key: ");
-            key = scanner.nextInt();
-            scanner.nextLine();
-            String requestData;
-
-            switch (Query.RequestNumber.fromInt(key)) {
-                case BY_SHORT_NAME:
-                    System.out.println("Enter shortName: ");
-                    String shortName = scanner.nextLine();
-                    requestData = shortName;
-                    result = Query.findByShortName(companyList, shortName);
-                    break;
-                case BY_INDUSTRY:
-                    System.out.println("Enter industry: ");
-                    String industry = scanner.nextLine();
-                    requestData = industry;
-                    result = Query.findByIndustry(companyList, industry);
-                    break;
-                case BY_ACTIVITY:
-                    System.out.println("Enter activity: ");
-                    String activity = scanner.nextLine();
-                    requestData = activity;
-                    result = Query.findByActivity(companyList, activity);
-                    break;
-                case BY_FOUNDATION_DATE:
-                    try {
-                        System.out.println("Enter 2 dates(from dd.mm.yyyy to dd.mm.yyyy): ");
-                        requestData = scanner.nextLine();
-                        String[] dates = requestData.split(" ");
-                        Date date1 = Company.format.parse(dates[0]);
-                        Date date2 = Company.format.parse(dates[1]);
-                        result = Query.findByFoundationDate(companyList, date1, date2);
-                    } catch (Exception ex) {
-                        throw new CustomException("Reading data failed :: " + ex);
-                    }
-                    break;
-                case BY_EMPLOYEE_NUMBER:
-                    try {
-                        System.out.println("Enter 2 numbers(from .. to):");
-                        requestData = scanner.nextLine();
-                        String[] nums = requestData.split(" ");
-                        int num1 = Integer.parseInt(nums[0]);
-                        int num2 = Integer.parseInt(nums[1]);
-                        result = Query.findByEmployeeNumber(companyList, num1, num2);
-                    } catch (Exception ex) {
-                        throw new CustomException("Reading data failed :: " + ex);
-                    }
-                    break;
-                default:
-                    LOGGER.fine("EXIT\n");
-                    return;
-            }
-
-            System.out.println("Companies found: ");
-            try {
-                if (result.isEmpty()) {
-                    of.write(("None\n"));
-                } else {
-                    for (Company company : result)
-                        of.write(company.toString() + System.lineSeparator());
-                }
-                of.write(System.lineSeparator());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            LOGGER.fine("Find company " + Query.RequestNumber.fromInt(key) + "::" + requestData +
-                    "\n\t\tCompanies found: " + result.size());
-        }
-    }
-
-    static void printMenu() {
-        System.out.println(
-                "1. Найти компанию по краткому наименованию.\n" +
-                        "2. Выбрать компании по отрасли.\n" +
-                        "3. Выбрать компании по виду деятельности.\n" +
-                        "4. Выбрать компании по дате основания в определенном промежутке (с и по).\n" +
-                        "5. Выбрать компании по численности сотрудников в определенном промежутке (с и по).\n" +
-                        "0. End.\n" +
-                        "-------------------"
-        );
-    }
 }
